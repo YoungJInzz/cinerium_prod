65629574b6f0b8b05e259b8916c47b45cb3d17ee
DongKyoungPark <oe1lolo@nate.com>
2020-03-30 11:09:27 +0900

    Basic Setting

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Nav from "./components/navigation";
 
 function App() {
-  return <div className="App">start from here</div>;
+  return (
+    <div className="App">
+      <>
+        <Nav />
+      </>
+    </div>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import "./style/reset.css";
+import "./style/style.css";
+import "./font/font.css";
 
 ReactDOM.render(
   <React.StrictMode>
