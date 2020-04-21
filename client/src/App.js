c8490e9ea70392eef73745d03b2ee1796b020bbf
DongKyoung <e1loldan@gmail.com>
2020-04-21 14:03:26 +0900

    MainPage Finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
 import React, { memo } from "react";
 import Navigation from "./components/navigation";
 import Main from "./components/main";
+import Serch from "./components/serch"
 import Footer from "./components/footer";
 import BookingPage from "./pages/bookingpage";
+import Signup from "./components/signup";
 
 const App = memo(() => {
   return (
     <>
-      <div className="App">
-        <>
-          <Navigation />
-          <Main />
-          <Footer />
-        </>
-      </div>
-
       <div>
-      <BookingPage />
+        <Navigation />
+        {/* <Signup/> */}
+        <Main />
+        <Serch/>
+        <Footer />
       </div>
-    </>    
+      
+      <div>
+        <BookingPage />
+      </div>
+    </>
   );
 });
 
