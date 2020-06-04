edf87a2edf86dd94e283f46f19f11bf56bf43930
YoungJInzz <dudwls3320@gmail.com>
2020-06-04 17:49:17 +0900

    Modify main

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Logo from "../resources/logowhite.svg";
 import { FaBars } from "react-icons/fa";
+import { IoMdPower } from "react-icons/io";
+import { BsCircleFill, BsCircle } from "react-icons/bs";
 const Header = () => {
   const [step, setStep] = useState("");
   return (
@@ -14,16 +16,41 @@ const Header = () => {
         </nav>
       </header>
       <aside className="main-sidebar">
+        <section className="login">
+          <div className="loginBtn">
+            <IoMdPower className="powerIcon" />
+          </div>
+          <div className="loginForm">
+            <input className="loginInput" type="text" placeholder="id"></input>
+            <input
+              className="loginInput"
+              type="password"
+              placeholder="password"
+            ></input>
+          </div>
+        </section>
         <section className="sidebar">
           <ul>
-            <li className="nav-li">영화</li>
-            <li className="nav-li">예매</li>
+            <li className="nav-li">
+              <BsCircleFill className="circleFillIcon" />
+              영화(개발중)
+            </li>
+            <li className="nav-li">
+              <BsCircleFill className="circleFillIcon" />
+              예매
+            </li>
             <ul>
               <li>빠른예매</li>
               <li>상영시간표</li>
             </ul>
-            <li className="nav-li">극장</li>
-            <li className="nav-li">이벤트</li>
+            <li className="nav-li">
+              <BsCircleFill className="circleFillIcon" />
+              극장(개발중)
+            </li>
+            <li className="nav-li">
+              <BsCircleFill className="circleFillIcon" />
+              이벤트(개발중)
+            </li>
           </ul>
         </section>
       </aside>
