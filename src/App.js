2d470a568db7037e6447b298dd11e70d6483dd9d
YoungJInzz <dudwls3320@gmail.com>
2020-06-09 00:15:42 +0900

    Add login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React, { memo } from "react";
 import Header from "./components/header";
-import Navigation from "./components/navigation";
-import Main from "./components/main";
-import Serch from "./components/serch";
-import Footer from "./components/footer";
-import BookingPage from "./pages/bookingpage";
-import Signup from "./components/signup";
 import { Route, Switch } from "react-router-dom";
 import TimetableByCinemaContainer from "./containers/timetableByCinemaContainer";
 import BookingContainer from "./containers/bookingContainer";
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 import Logo from "../resources/logowhite.svg";
+import { Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
 import { IoMdPower } from "react-icons/io";
 import { BsCircleFill, BsCircle } from "react-icons/bs";
 const Header = () => {
   const [step, setStep] = useState("");
+  const handleStep = (x) => {
+    if (step === x) {
+      setStep("");
+    } else setStep(x);
+  };
   return (
     <div>
       <header className="main-header">
@@ -29,19 +35,26 @@ const Header = () => {
             ></input>
           </div>
         </section>
+        <div className="menuTitle"> Menu</div>
         <section className="sidebar">
           <ul>
             <li className="nav-li">
               <BsCircleFill className="circleFillIcon" />
               영화(개발중)
             </li>
-            <li className="nav-li">
+            <li className="nav-li" onClick={() => handleStep("booking")}>
               <BsCircleFill className="circleFillIcon" />
               예매
             </li>
-            <ul>
-              <li>빠른예매</li>
-              <li>상영시간표</li>
+            <ul className={step === "booking" ? " show" : "hide"}>
+              <li className="nav-child-li">
+                <BsCircle className="circleIcon" />
+                빠른예매
+              </li>
+              <li className="nav-child-li">
+                <BsCircle className="circleIcon" />
+                상영시간표
+              </li>
             </ul>
             <li className="nav-li">
               <BsCircleFill className="circleFillIcon" />
